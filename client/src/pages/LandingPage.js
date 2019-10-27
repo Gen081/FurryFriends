ecb393fe85801f7742d4ef3904b8593d926de304
Andres Urdaneta <aeum3893@gmail.com>
2019-10-27 16:37:40 -0400

    created assets dir within the src dir with the stylesheets for the navbar and landingpage and refactored code

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../assets/stylesheets/landingpage.css'
 
 const LandingPage = () => (
   <div>
     <div>
-      <img src={"images/dog.jpg"} alt={"Dog"} />
-      <button class="center-btn">Find your Companion!</button>
+      <img className="dog-image" src={"images/dog.jpg"} alt={"Dog"} />
+      <button className="center-btn">Find your Companion!</button>
     </div>
     <p className="about">Connecting adoptable pets with families.</p>
   </div>
