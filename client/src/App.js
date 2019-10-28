52ae71b695b64146b6fbb6a665b43ef72f7e93a0
vaznatalia <natalialuizavaz@gmail.com>
2019-10-28 15:07:55 -0400

    fix missing import

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,8 @@ import './App.css'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import LandingPage from './pages/LandingPage'
-<<<<<<< HEAD
 import dogsForAdoption from './pages/dogsForAdoption'
-=======
-import PageTwo from './pages/PageTwo'
 import ShowAnimal from './pages/ShowAnimal'
->>>>>>> c255908d20eb609fe279390808b2af3ded432f60
 
 class App extends React.Component {
   state = {}
