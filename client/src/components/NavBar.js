b3e7701dd92f2c224a39351b0152874ebf27753e
Valiana <valiana.josic@gmail.com>
2019-10-29 13:37:01 -0400

    Update Nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import '../assets/stylesheets/navbar.css';
+import '../assets/stylesheets/navbar.css'
+import Paw from '../assets/images/paw.png'
+import Peek from '../assets/images/peek.jpg'
 
 const Navbar = () => (
   <nav>
     <ul>
-      <li> <img className="paw" src={"images/paw.png"} alt={"paw"} /></li>
+      <li> <img className="paw" src={Paw} alt={"paw"} /></li>
       <li><h1>FurryFriendFinder</h1></li>
       <li className="slogan">Adopt a Friendly Companion</li>
     </ul>
     <div className="btn">
-      <img className="peek" src={"images/peek.jpg"} alt={"peek"} />
+      <img className="peek" src={Peek} alt={"peek"} />
       <button>Services</button>
       <button>Resouces</button>
       <button>LogIn</button>
