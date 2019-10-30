02a48492babea208d5218eb62fd1e96bd2149351
Andres Urdaneta <aeum3893@gmail.com>
2019-10-29 22:57:42 -0400

    contactWrapper to the contact form component

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -4,51 +4,51 @@ import '../assets/stylesheets/ContactForm.css'
 
 const ContactForm = (props) => {
 
-    return(
-      <div> 
-        <h1 className="form-field header">Contact Me!</h1>
-        <form id='ContactForm'action="https://formspree.io/pierrelouisgenny@gmail.com" method="POST">
-          <div className="form-input">
-            <label htmlFor="your-name">Your Name</label>
-            <input
-              id="your-name"
-              type="text"
-              name="your-name"
-              maxLength="60"
-              autoComplete="off"
-              className="form-field name-input"
-            />
-          </div>         
-                 
-          <div className="form-input">
-            <label htmlFor="your-email">Your email</label>
-            <input 
-              id="your-email" 
-              type="text"
-              name="your-email"
-              maxLength="60"
-              autoComplete="off"
-              className="form-field email-input"
-            />
-          </div>
-          <div className="form-input">
-            <label htmlFor="your-message">Your Message</label>
-            <textarea id="your-message"
-              name="your-message"
-              cols="40"
-              rows="10"
-              className="form-field message-text-area"
-              />
-          </div>
-          <div className="form-input">
-            <button type="submit">Send</button>
-            <button type="reset">Reset</button>
-            {/* <input type="reset"/> */}
-          </div>
-        </form >
-      </div>        
-    )
-  }
+  return (
+    <div className="contactWrapper">
+      <h1 className="form-field header">Contact Me!</h1>
+      <form id='ContactForm' action="https://formspree.io/pierrelouisgenny@gmail.com" method="POST">
+        <div className="form-input">
+          <label htmlFor="your-name">Your Name</label>
+          <input
+            id="your-name"
+            type="text"
+            name="your-name"
+            maxLength="60"
+            autoComplete="off"
+            className="form-field name-input"
+          />
+        </div>
+
+        <div className="form-input">
+          <label htmlFor="your-email">Your email</label>
+          <input
+            id="your-email"
+            type="text"
+            name="your-email"
+            maxLength="60"
+            autoComplete="off"
+            className="form-field email-input"
+          />
+        </div>
+        <div className="form-input">
+          <label htmlFor="your-message">Your Message</label>
+          <textarea id="your-message"
+            name="your-message"
+            cols="40"
+            rows="10"
+            className="form-field message-text-area"
+          />
+        </div>
+        <div className="form-input">
+          <button type="submit">Send</button>
+          <button type="reset">Reset</button>
+          {/* <input type="reset"/> */}
+        </div>
+      </form >
+    </div>
+  )
+}
 
 export default ContactForm
 
