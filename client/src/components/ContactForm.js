6e281578dd82771f0da599d2886fbb56924fb2d7
Andres Urdaneta <aeum3893@gmail.com>
2019-10-30 10:05:08 -0400

    used react bootstrap to add styling to the contact form

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,51 +1,32 @@
 import React from 'react'
 import '../assets/stylesheets/ContactForm.css'
-
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const ContactForm = (props) => {
 
   return (
     <div className="contactWrapper">
-      <h1 className="formHeader">Contact Me!</h1>
-      <form id='contactForm' action="https://formspree.io/pierrelouisgenny@gmail.com" method="POST">
-        <div className="form-input">
-          <label htmlFor="your-name">Your Name</label>
-          <input
-            id="your-name"
-            type="text"
-            name="your-name"
-            maxLength="60"
-            autoComplete="off"
-            className="form-field name-input"
-          />
-        </div>
-
-        <div className="form-input">
-          <label htmlFor="your-email">Your email</label>
-          <input
-            id="your-email"
-            type="text"
-            name="your-email"
-            maxLength="60"
-            autoComplete="off"
-            className="form-field email-input"
-          />
-        </div>
-        <div className="form-input">
-          <label htmlFor="your-message">Your Message</label>
-          <textarea id="your-message"
-            name="your-message"
-            cols="40"
-            rows="10"
-            className="form-field message-text-area"
-          />
-        </div>
-        <div className="form-input">
-          <button type="submit">Send</button>
-          <button type="reset">Reset</button>
-          {/* <input type="reset"/> */}
-        </div>
-      </form >
+      <div className="contactHeader">
+        <h1>Adopt this pet...</h1>
+        <h2>Get in Touch!</h2>
+      </div>
+      <Form>
+        <Row>
+          <Col>
+            <Form.Control placeholder="Name" />
+          </Col>
+          <Col>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Col>
+        </Row>
+        <Row className="message">
+          <Col>
+            <Form.Control as="textarea" rows="3" placeholder="Message" />
+          </Col>
+        </Row>
+      </Form>
     </div>
   )
 }
